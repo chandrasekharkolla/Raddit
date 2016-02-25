06830de1998b12796f9cb8aed0634683eb3b6dd5
chandrasekharkolla <chandrasekharkolla@gmail.com>
2016-02-25 15:36:31 -0500

    Add comment functionality

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :comments
   devise_for :users, :controllers => { registrations: 'registrations' }
   root 'links#index'
 
@@ -7,7 +8,9 @@ Rails.application.routes.draw do
       put "like",    to: "links#upvote"
       put "dislike", to: "links#downvote"
     end
+    resources :comments
   end
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
